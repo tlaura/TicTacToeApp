d1e52289d31639d3afa0d2b512f0d924305261c7
Laura <tundelaura93@gmail.com>
2019-06-20 10:31:33 -0400

    main class

--- src/tictactoeapplication/TicTacToeApp.java
+++ src/tictactoeapplication/TicTacToeApp.java
@@ -7,11 +7,18 @@ public class TicTacToeApp {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-//		allows for continuous game:
+//		Allows for continuous game:
 		boolean doYouWantToPlay = true;
 		while(doYouWantToPlay) {
-			System.out.println("Welcome to Tic Tac Toe!\n You must pick which character you want to be"
-					+ "and which character the computer will be.");
+			System.out.println("Welcome to Tic Tac Toe!\n ");
+			System.out.println();
+			System.out.println("Enter a single character that will represent you on the board: ");
+			char playerToken = scan.next().charAt(0);
+			System.out.println("Enter a single character that will represent the computer on the board: ");
+			char computerToken = scan.next().charAt(0);
+			
+			TicTacToe game = new TicTacToe(playerToken, computerToken);
+			
 		}
 		
 	}
